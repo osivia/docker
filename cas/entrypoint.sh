89e6de7c8ce221291418ae71f2384ff73875661b
loic <loic@bebop>
2018-10-19 12:22:10 +0200

    Env foad, préserve les crt, ajout onlyoffice et libreoffice

--- cas/entrypoint.sh
+++ cas/entrypoint.sh
@@ -5,15 +5,15 @@ set -e
 LDAP_HOST=${LDAP_HOST:-opendj}
 LDAP_PORT=${LDAP_PORT:-1389}
 
-CAS_PROPERTIES=$CAS_HOME/cas-config.properties
+CAS_PROPERTIES=$CAS_CONFIG/cas.properties
 
-if [ ! -f $CAS_HOME/configured ]; then
+if [ ! -f $CAS_CONFIG/configured ]; then
     echo "Configuration..."
 
     sed -i s\\^[#]*ldap.url=.*$\\#ldap.url\\g $CAS_PROPERTIES
 	echo "ldap.url=ldap://${LDAP_HOST}:${LDAP_PORT}" >> $CAS_PROPERTIES
 
-    touch $CAS_HOME/configured
+    touch $CAS_CONFIG/configured
 fi    
 
 catalina.sh run
