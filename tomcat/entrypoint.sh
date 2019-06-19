74f42c1f73c9195f82d4e7838899ff8b44f4d71c
Loïc Billon <lbillon@osivia.com>
2019-06-19 09:26:15 +0200

    Suppr. visionneuse, dépl. fichiers de conf vers /etc

--- tomcat/entrypoint.sh
+++ tomcat/entrypoint.sh
@@ -7,23 +7,23 @@ PUBLIC_HOST=${PUBLIC_HOST:-"localhost"}
 LDAP_HOST=${LDAP_HOST:-opendj}
 LDAP_PORT=${LDAP_PORT:-1389}
 
-CAS_PROPERTIES=$CAS_CONFIG/cas.properties
+CAS_PROPERTIES=/etc/cas/cas.properties
 
-if [ ! -f $CAS_CONFIG/configured ]; then
-	
+
+if [ ! -f $CATALINA_HOME/configured ]; then
 	
     echo "Configuration..."
-
+    
+	sed -i s\\PUBLIC_HOST\\$PUBLIC_HOST\\g $CAS_PROPERTIES
     sed -i s\\^[#]*ldap.url=.*$\\#ldap.url\\g $CAS_PROPERTIES
 	echo "ldap.url=ldap://${LDAP_HOST}:${LDAP_PORT}" >> $CAS_PROPERTIES
-
-    
+		
     echo "Register public cert in keystore for https client..."
     
     openssl pkcs12 -export -in /export/httpd/ssl/server.crt -inkey /export/httpd/ssl/server.key -out /etc/ssl/server.p12 -name ${PUBLIC_HOST} -password pass:osivia -caname root  || { exit 1; }
 	keytool -importkeystore -deststorepass changeit -destkeypass changeit -destkeystore ${JAVA_HOME}/lib/security/cacerts -srckeystore /etc/ssl/server.p12 -srcstoretype PKCS12 -srcstorepass osivia -alias ${PUBLIC_HOST} || { exit 1; }
 	
-    touch $CAS_CONFIG/configured
+    touch $CATALINA_HOME/configured
 	
 fi    
 
