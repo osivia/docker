ad6a3a3514992c83a18cbf51012e228afc43891a
Cédric Krommenhoek <cedric.krommenhoek@gmail.com>
2023-06-26 16:39:28 +0200

    📦️ Update composition

---
+++ apis/entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+if [ ! -f $CATALINA_HOME/configured ]; then
+    echo "Configuration..."
+
+
+    touch $CATALINA_HOME/configured
+fi    
+
+#mkdir -p $CATALINA_HOME/conf/Catalina/localhost
+#mv $CATALINA_HOME/ws-toutatice-content-store.xml $CATALINA_HOME/conf/Catalina/localhost
+
+catalina.sh jpda run
